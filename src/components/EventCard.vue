bf5bb380d6e2db00388391caa29416ba48f9e939
Milan <mmatic@hispatecanalytics.com>
2021-02-03 00:44:00 +0100

    Initial commit + Lesson 3

---
+++ src/components/EventCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="event-card">
+    <span>@ {{ props.event.time }} on {{ event.date }}</span>
+    <h4>{{ props.event.title }}</h4>
+  </div>
+</template>
+
+<script setup>
+import { reactive, defineProps } from "vue";
+
+const props = defineProps({
+  event: {
+    type: Object,
+    required: true
+  }
+});
+</script>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
